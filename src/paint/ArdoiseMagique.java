a7ec538b2fac24921715ce135bd6c7c5227f1a0f
Joris-be <joris.beldjoudi@gmail.com>
2025-03-24 16:25:17 +0100

    ARDOISE-MAGIQUE VERSION OK
    BOUTON EFFACER MARCHE !!!!!!!!!!!!!

--- src/paint/ArdoiseMagique.java
+++ src/paint/ArdoiseMagique.java
@@ -1,68 +1,77 @@
 package paint;
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Point;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author joris
+ */
 
 
 public class ArdoiseMagique extends JFrame {
     private Color couleurActuelle = Color.RED;  // Couleur de départ (rouge)
-    private DrawingArea areaDessin;
+    private Point debut = null;
 
     public ArdoiseMagique() {
         setTitle("Ardoise Magique");
         setSize(600, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-
+        
         // Panel principal
         JPanel panelPrincipal = new JPanel(new BorderLayout());
         add(panelPrincipal);
-
+        
         // Zone de dessin
-        areaDessin = new DrawingArea();
+        DrawingArea areaDessin = new DrawingArea();
         panelPrincipal.add(areaDessin, BorderLayout.CENTER);
-
+        
         // Barre d'outils
         JPanel toolbar = new JPanel();
         panelPrincipal.add(toolbar, BorderLayout.NORTH);
-
-        // Boutons de couleur
+        
+        // Boutons de couleur (niveau facile)
         JButton boutonRouge = new JButton("Rouge");
-        boutonRouge.addActionListener(e -> areaDessin.setCouleur(Color.RED));
+        boutonRouge.addActionListener(e -> couleurActuelle = Color.RED);
         toolbar.add(boutonRouge);
-
+        
         JButton boutonVert = new JButton("Vert");
-        boutonVert.addActionListener(e -> areaDessin.setCouleur(Color.GREEN));
+        boutonVert.addActionListener(e -> couleurActuelle = Color.GREEN);
         toolbar.add(boutonVert);
-
+        
         JButton boutonBleu = new JButton("Bleu");
-        boutonBleu.addActionListener(e -> areaDessin.setCouleur(Color.BLUE));
+        boutonBleu.addActionListener(e -> couleurActuelle = Color.BLUE);
         toolbar.add(boutonBleu);
-
-        // Bouton pour ouvrir une palette de couleurs complète
+        
+        // Ajouter un bouton pour ouvrir une palette de couleurs complète (niveau difficile)
         JButton boutonPalette = new JButton("Palette Complète");
         boutonPalette.addActionListener(e -> {
-            Color nouvelleCouleur = JColorChooser.showDialog(this, "Choisir une couleur", couleurActuelle);
-            if (nouvelleCouleur != null) {
-                areaDessin.setCouleur(nouvelleCouleur);
-            }
+            // Ouvre un JColorChooser pour permettre à l'utilisateur de choisir n'importe quelle couleur
+            couleurActuelle = JColorChooser.showDialog(this, "Choisir une couleur", couleurActuelle);
         });
         toolbar.add(boutonPalette);
-
+        
         // Bouton pour effacer
         JButton boutonEffacer = new JButton("Effacer");
         boutonEffacer.addActionListener(e -> areaDessin.clear());
         toolbar.add(boutonEffacer);
-
+        
         // Gomme
         JButton boutonGomme = new JButton("Gomme");
         boutonGomme.addActionListener(e -> areaDessin.activerGomme());
         toolbar.add(boutonGomme);
-
+        
         setVisible(true);
     }
-
+    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new ArdoiseMagique());
     }
 }
-
--- src/paint/DrawingArea.java
+++ src/paint/DrawingArea.java
@@ -2,73 +2,60 @@ package paint;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Stack;
+import javax.swing.JPanel;
 
 public class DrawingArea extends JPanel {
     private Color couleurActuelle = Color.RED;
-    private boolean gommeActive = false;
     private Point debut = null;
-    private BufferedImage image;
-    private Graphics2D g2d;
-
+    private boolean gommeActive = false;
+    private Stack<Image> historiqueImages = new Stack<>();
+    
     public DrawingArea() {
         setBackground(Color.WHITE);
         setPreferredSize(new Dimension(600, 300));
-
-        // Initialiser l'image de dessin
-        image = new BufferedImage(600, 300, BufferedImage.TYPE_INT_ARGB);
-        g2d = image.createGraphics();
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, 600, 300); // Remplir l'image avec du blanc
-        g2d.setColor(couleurActuelle);
-
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
                 debut = e.getPoint();
             }
         });
-
         addMouseMotionListener(new MouseAdapter() {
             public void mouseDragged(MouseEvent e) {
                 if (debut != null) {
+                    Graphics g = getGraphics();
                     if (gommeActive) {
-                        g2d.setColor(Color.WHITE);
-                        g2d.fillRect(e.getX() - 5, e.getY() - 5, 10, 10);
+                        g.setColor(Color.WHITE);  // Gomme = fond blanc
+                        g.fillRect(e.getX() - 5, e.getY() - 5, 10, 10);
                     } else {
-                        g2d.setColor(couleurActuelle);
-                        g2d.drawLine(debut.x, debut.y, e.getX(), e.getY());
+                        g.setColor(couleurActuelle);
+                        g.drawLine(debut.x, debut.y, e.getX(), e.getY());
                     }
                     debut = e.getPoint();
-                    repaint(); // Redessiner la zone
                 }
             }
         });
     }
 
-    public void setCouleur(Color couleur) {
-        this.couleurActuelle = couleur;
-        this.gommeActive = false;
+    public void clear() {
+        historiqueImages.clear();
+        repaint();
     }
 
     public void activerGomme() {
         gommeActive = !gommeActive;
     }
 
-    public void clear() {
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, getWidth(), getHeight()); // Effacer tout
-        g2d.setColor(couleurActuelle);
-        repaint();
-    }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, this);
+        // Repeindre l'historique (si besoin de restaurer l'image après effacement)
+        if (!historiqueImages.isEmpty()) {
+            g.drawImage(historiqueImages.peek(), 0, 0, this);
+        }
     }
 }
 
